5f1877b37df58cea71b75a4652153f79a51a765f
mrudula1116 <poremrudula@gmail.com>
2026-08-26 18:26:25 +0530

    chore: configure code quality tooling

--- packages/mobile-lib/jest.config.js
+++ packages/mobile-lib/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'react-native',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+
+  transformIgnorePatterns: [
+    'node_modules/(?!((@react-native|react-native)/|\\.pnpm/))',
+  ],
 };
