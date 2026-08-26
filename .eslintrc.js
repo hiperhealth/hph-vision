d5df92cc7e65dcc2a0eea201609d528b0a420a6c
mrudula1116 <poremrudula@gmail.com>
2026-08-26 17:08:39 +0530

    chore: configure code quality tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'coverage/',
+    '.expo/',
+    '.metro/',
+    '.cache/',
+  ],
+  overrides: [
+    {
+      files: ['packages/mobile-lib/**/*.ts', 'packages/mobile-lib/**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './packages/mobile-lib/tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
 };
